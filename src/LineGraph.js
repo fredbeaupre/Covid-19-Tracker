69f3406863592e66ddaff881bceb10d0120e6f31
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2020-08-26 12:10:02 -0400

    First commit

---
+++ src/LineGraph.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType, ...props }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+        });
+    };
+    fetchData();
+  }, [casesType]);
+
+  const setDataColor = (casesType) => {
+    if (casesType === "cases") {
+      return "rgba(255, 215, 0, 0.8)";
+    }
+    if (casesType === "recovered") {
+      return "rgba(0, 255, 0, 0.5)";
+    }
+    if (casesType === "deaths") {
+      return "rgba(255, 0, 0, 0.5)";
+    }
+  };
+
+  return (
+    <div className={props.className}>
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: setDataColor(casesType),
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
